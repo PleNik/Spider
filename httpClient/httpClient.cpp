5d385d780b563a35318c2f1519028cacd8cefa75
PleNik <pleshkovn69@gmail.com>
2025-08-26 20:12:42 +0500

    httpClient and gitignore modified

--- httpClient/httpClient.cpp
+++ httpClient/httpClient.cpp
@@ -19,8 +19,6 @@ int main()
 
 		std::string startPage = parserIniFile.getStartPage(); //получили стартовую станицу
 		
-		DataBase dataBase(parserIniFile); //создали в базе данных таблицы
-
 		UrlAddress link = sharedAddress(startPage); //сохранили порт, хост и таргет в поля структуры UrlAddress
 
 		int depth = parserIniFile.getRecursionDepth();
@@ -63,82 +61,88 @@ std::string getHtmlPage(const UrlAddress& address)
 {
 	std::string htmlPage;
 
-	std::string host = address.hostName;
-	std::string target = address.target;
+	try {
+		std::string host = address.hostName;
+		std::string target = address.target;
 
-	net::io_context ioc;
+		net::io_context ioc;
 
-	if (address.protocol == ProtocolType::HTTP) {
+		if (address.protocol == ProtocolType::HTTP) {
 
-		tcp::resolver resolver(ioc);
-		beast::tcp_stream stream(ioc);
+			tcp::resolver resolver(ioc);
+			beast::tcp_stream stream(ioc);
 
-		//Поиск доменного имени
-		auto const results = resolver.resolve(host, "http");
+			//Поиск доменного имени
+			auto const results = resolver.resolve(host, "http");
 
-		//Соединение по IP -адресу, который мы получаем от поиска
-		stream.connect(results);
+			//Соединение по IP -адресу, который мы получаем от поиска
+			stream.connect(results);
 
-		//Настройка сообщения запроса HTTP GET
-		http::request<http::string_body> req{ http::verb::get, target, 11 };
-		req.set(http::field::host, host);
-		req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+			//Настройка сообщения запроса HTTP GET
+			http::request<http::string_body> req{ http::verb::get, target, 11 };
+			req.set(http::field::host, host);
+			req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
 
-		//Отправить HTTP -запрос на удаленный хост
-		http::write(stream, req);
+			//Отправить HTTP -запрос на удаленный хост
+			http::write(stream, req);
 
-		//Буфер для чтения
-		beast::flat_buffer buffer;
+			//Буфер для чтения
+			beast::flat_buffer buffer;
 
-		//Объявляем контейнер для ответа
-		http::response<http::dynamic_body> res;
+			//Объявляем контейнер для ответа
+			http::response<http::dynamic_body> res;
 
-		//Получаем ответ HTTP
-		http::read(stream, buffer, res);
+			//Получаем ответ HTTP
+			http::read(stream, buffer, res);
 
-		htmlPage = buffers_to_string(res.body().data());
+			htmlPage = buffers_to_string(res.body().data());
 
-		std::cout << res << std::endl;
 
-		//Закрываем соккет
-		beast::error_code ec;
-		stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+			//Закрываем соккет
+			beast::error_code ec;
+			stream.socket().shutdown(tcp::socket::shutdown_both, ec);
 
-		//Если нет соединения
-		if (ec && ec != beast::errc::not_connected) {
-			throw beast::system_error{ ec };
+			//Если нет соединения
+			if (ec && ec != beast::errc::not_connected) {
+				throw beast::system_error{ ec };
+			}
+		}
+		else {
+			//обработка https-протокола
+
+			ssl::context ctx(ssl::context::sslv23_client);
+			ctx.set_default_verify_paths();
+
+			beast::ssl_stream<beast::tcp_stream> stream(ioc, ctx);
+			stream.set_verify_mode(ssl::verify_none);
+
+			stream.set_verify_callback([](bool preverified, ssl::verify_context& ctx) {
+				return true;
+				});
+
+			ip::tcp::resolver resolver(ioc);
+			get_lowest_layer(stream).connect(resolver.resolve(host, "https"));
+			get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
+
+			http::request<http::empty_body> req{ http::verb::get, target, 11 };
+			req.set(http::field::host, host);
+			req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+			SSL_set_tlsext_host_name(stream.native_handle(), host.c_str());
+
+			stream.handshake(ssl::stream_base::client);
+			http::write(stream, req);
+
+			beast::flat_buffer buffer;
+			http::response<http::dynamic_body> res;
+			http::read(stream, buffer, res);
+
+			htmlPage = buffers_to_string(res.body().data());
+
 		}
 	}
-	else {
-		//обработка https-протокола
-
-		ssl::context ctx(ssl::context::sslv23_client);
-		ctx.set_default_verify_paths();
-
-		beast::ssl_stream<beast::tcp_stream> stream(ioc, ctx);
-		stream.set_verify_mode(ssl::verify_none);
-
-		stream.set_verify_callback([](bool preverified, ssl::verify_context& ctx) {
-			return true;
-		});
-
-		ip::tcp::resolver resolver(ioc);
-		get_lowest_layer(stream).connect(resolver.resolve(host, "https" ));
-		get_lowest_layer(stream).expires_after(std::chrono::seconds(30));
-
-		http::request<http::empty_body> req{ http::verb::get, target, 11 };
-		req.set(http::field::host, host);
-		req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-
-		stream.handshake(ssl::stream_base::client);
-		http::write(stream, req);
-
-		beast::flat_buffer buffer;
-		http::response<http::dynamic_body> res;
-		http::read(stream, buffer, res);
-
-		htmlPage = buffers_to_string(res.body().data());
-
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
 	}
 
 	return htmlPage;
@@ -163,43 +167,48 @@ void  poolOfThreadsWorker() {
 
 void linkParser(DatabaseWorker& databaseWorker, UrlAddress link, int depth, int index, int ofTotalCount)
 {
-	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	try {
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 
-	std::string htmlPage = getHtmlPage(link);
+		std::string htmlPage = getHtmlPage(link);
 
-	std::vector<std::string> rawLinks = pickOutLinks(htmlPage);
+		std::vector<std::string> rawLinks = pickOutLinks(htmlPage);
 
-	std::unordered_set<UrlAddress> linksUnique = uniqueLinks(rawLinks, link.protocol,
-		link.hostName);
+		std::unordered_set<UrlAddress> linksUnique = uniqueLinks(rawLinks, link.protocol,
+			link.hostName);
 
 
-	std::string text = indexer(htmlPage); //индексация веб-страницы
+		std::string text = indexer(htmlPage); //индексация веб-страницы
 
-	//запись слов текста в вектор
-	std::vector<std::string> wordsVec = splitText(text);
+		//запись слов текста в вектор
+		std::vector<std::string> wordsVec = splitText(text);
 
-	//подсчет слов в тексте
-	std::map<std::string, int> wordsCount = countWords(wordsVec);
+		//подсчет слов в тексте
+		std::map<std::string, int> wordsCount = countWords(wordsVec);
 
-	databaseWorker.addWords(link, wordsCount);
+		databaseWorker.addWords(link, wordsCount);
 
-	if (depth > 0) {
-		std::lock_guard<std::mutex> lock(mtx);
+		if (depth > 0) {
+			std::lock_guard<std::mutex> lock(mtx);
 
-		size_t count = linksUnique.size();
-		size_t index = 0;
-		for (auto& link : linksUnique)
-		{
-			bool docExists = databaseWorker.documentExists(link);
-			if (!docExists)
+			size_t count = linksUnique.size();
+			size_t index = 0;
+			for (auto& link : linksUnique)
 			{
-				tasks.push([&databaseWorker, link, depth, index, count]() {
-					linkParser (databaseWorker, link, depth - 1, index, count); 
-				});
-				index++;
+				bool docExists = databaseWorker.documentExists(link);
+				if (!docExists)
+				{
+					tasks.push([&databaseWorker, link, depth, index, count]() {
+						linkParser(databaseWorker, link, depth - 1, index, count);
+						});
+					index++;
+				}
 			}
+			cv.notify_one();
 		}
-		cv.notify_one();
 	}
-
+	catch (const std::exception& ex) {
+		std::cout << ex.what() << std::endl;
+	}
+	
 }
